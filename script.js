81457eac442000e9b0aaad2a24a8862afb6c3f5f
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-01 15:18:34 +0100

    bonne voie

---
+++ script.js
@@ -0,0 +1,17 @@
+
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET','https://thatsthespir.it/api',true);
+
+    xhr.onload = () => {
+
+        if(xhr.status === 200){
+            console.log(JSON.parse(xhr.response));
+        };
+
+        let result = JSON.parse(xhr.response);
+
+        document.querySelector('#quote').innerHTML = result.quote;
+        document.querySelector('#photo').src = result.photo;
+    };
+
+    xhr.send();
