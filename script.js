418755b9ff1126b3453910d23f8ab19d775faa4b
DeceulaerAdrien <adriendeceulaer@gmail.com>
2021-03-04 09:56:27 +0100

    04/03 09:56

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-    let xhr = new XMLHttpRequest;
-    xhr.open('GET','https://thatsthespir.it/api',true);
+let xhr = new XMLHttpRequest;
+xhr.open('GET', 'https://thatsthespir.it/api', true);
 
-    xhr.onload = () => {
+xhr.onload = () => {
 
-        if(xhr.status === 200){
-            console.log(JSON.parse(xhr.response));
-        };
-
-        let result = JSON.parse(xhr.response);
-
-        document.querySelector('#quote').innerHTML = result.quote;
-        document.querySelector('#photo').src = result.photo;
-        document.querySelector('#name').innerHTML = result.author;
+    if (xhr.status === 200) {
+        console.log(JSON.parse(xhr.response));
     };
 
-    xhr.send();
+    let result = JSON.parse(xhr.response);
+
+    document.querySelector('#quote').innerHTML = result.quote;
+    document.querySelector('#photo').src = result.photo;
+    document.querySelector('#name').innerHTML = result.author;
+};
+
+xhr.send();
